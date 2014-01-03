b2e85f15ed889708dfbc4af4bb8e6221678a943c
jonathancg90 <jonathancg90@gmail.com>
2014-01-02 22:21:22 -0500

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YoutubeLearning::Application.config.secret_key_base = '7846985cb01baf496a6719370874cc07881121e2606fb5d56a674322cf7042a8d58bbd84a5726a083fd29427e9a3e428dce4e3f2a920fd88fe196c32f52f92c2'
